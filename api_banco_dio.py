73710e627162cd22eed8d501a5418da8b9a0ebf8
gustavofranco88 <gustavofranco88@gmail.com>
2023-06-02 20:42:22 -0300

    python + sqlite em andamento

---
+++ api_banco_dio.py
@@ -0,0 +1,148 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, create_engine
+from sqlalchemy import Numeric
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
+from sqlalchemy import select
+
+Base = declarative_base()
+
+
+class Clients(Base):
+    __tablename__ = "cliente"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    cpf = Column(String(9))
+    endereco = Column(String(40))
+
+    contas = relationship(
+        "Contas", back_populates="clientes", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self):
+        return f"Cliente = " \
+               f"conta: '{self.contas}', " \
+               f"nome: '{self.nome}', " \
+               f"cpf: '{self.cpf}', " \
+               f"endereço: '{self.endereco}'"
+
+
+class Contas(Base):
+    __tablename__ = "conta"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tipo = Column(String)
+    agencia = Column(String)
+    num = Column(Integer)
+    id_cliente = Column(Integer, ForeignKey("cliente.id"))
+    saldo = Column(Numeric(precision=10, scale=2))
+
+    clientes = relationship("Clients", back_populates="contas")
+
+    def __repr__(self):
+        return f"{self.clientes}, {self.tipo}, {self.agencia}, {self.num}, {self.saldo}"
+
+
+# Criando a engine de conexão com o DB
+engine = create_engine('sqlite://', echo=False)
+
+Base.metadata.create_all(engine)
+
+with Session(engine) as session:
+    pedro = Clients(
+        nome='pedro souza',
+        cpf='67854320638',
+        endereco='são paulo-sp brasil',
+        contas=[
+            Contas(tipo='poupança', agencia='1213', num=54789, saldo=125897.43)
+        ]
+    )
+
+    ana = Clients(
+        nome='ana souza',
+        cpf='02362423478',
+        endereco='são paulo-sp brasil',
+        contas=[
+            Contas(tipo='corrente', agencia='1213', num=54793, saldo=34200.00)
+        ]
+    )
+
+    fabio = Clients(
+        nome='fabio martins',
+        cpf='04836283983',
+        endereco='florianópolis-sc brasil',
+        contas=[
+            Contas(tipo='corrente', agencia='7222', num=100345, saldo=12567.32)
+        ]
+    )
+
+    rita = Clients(
+        nome='rita von hunt',
+        cpf='03483912789',
+        endereco='curitiba-pr brasil',
+        contas=[
+            Contas(tipo='poupança', agencia='7844', num=23445, saldo=2345.78)
+        ]
+    )
+
+    dexter = Clients(
+        nome='dexter araujo',
+        cpf='73490235673',
+        endereco='curitiba-pr brasil',
+        contas=[
+            Contas(tipo='poupança', agencia='7844', num=23789, saldo=879.54)
+        ]
+    )
+session.add_all([pedro, ana, rita, fabio, dexter])
+session.commit()
+
+
+
+session = Session(engine)
+'''
+# Selecionar todos os clientes da tabela
+statement = select(Clients)
+for user in session.scalars(statement):
+    print(user)
+
+# selecionar um cliente especifico pelo campo nome
+stmt = select(Clients).where(Clients.nome.in_(['ana souza']))
+for user in session.scalars(stmt):
+    print(user)'''
+
+# criando um join com as tabelas clientes e contas
+
+stmt_join = (
+    select(Contas.saldo, Clients.nome)
+    .join(Contas.clientes)
+    .where(Clients.endereco == 'curitiba-pr brasil')
+    .where(Contas.saldo >= 500.00)
+)
+
+# session.execute retorna todos os campos da tabela que eu queira
+clientes_curitiba = session.execute(stmt_join)
+for cliente in clientes_curitiba:
+    nome, saldo = cliente[1], cliente[0]
+    print(f"Nome: {nome.title()}, Saldo: R${saldo}")
+
+# session.scalars retorna apenas um campo específico
+for cliente in session.scalars(stmt_join):
+    print(f'Saldo {cliente}')
+
+# alterando dados
+
+stmt = select(Clients).where(Clients.nome == 'dexter araujo')
+dexter = session.scalars(stmt)
+dexter.nome = 'dexter araujo franco'
+session.commit()
+print(dexter.nome)
+
+stmt = select(Clients).where(Clients.nome == 'dexter araujo franco')
+for user in session.scalars(stmt):
+    print(user)
+'''
+for row in dexter:
+    row.nome = 'dois'
+'''
